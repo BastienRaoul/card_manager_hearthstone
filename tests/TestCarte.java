f5ed75eac15cce4e807ae7f95d03fec6c5afbd60
Swan <swan.royer@etu.univ-nantes.fr>
2018-06-18 17:39:03 +0200

    sl

--- tests/TestCarte.java
+++ tests/TestCarte.java
@@ -23,4 +23,30 @@ public class TestCarte {
         Cartes cartes = new Cartes(FabriqueJson.lireCartesDepuisFichier("json/initial.json"));
         assertEquals(3, cartes.collection().size());
     }
+
+    @Test
+    public void Classe() throws Exception {
+        Carte arme = new Arme("Marteau de Thor", 10, "Marteau de Thor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100,
+                100);
+        assertEquals("classe different", Classe.GUERRIER, arme.classe());
+    }
+
+    @Test
+    public void Rarete() throws Exception {
+        Carte arme = new Arme("Marteau de Thor", 10, "Marteau de Thor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100,
+                100);
+        assertEquals("rarete different", Rarete.LEGENDAIRE, arme.rarete());
+    }
+
+    @Test
+    public void nom() throws Exception {
+        Carte arme = new Arme("Marteau de Thor", 10, "Marteau de Thor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100,
+                100);
+        assertEquals("rarete different",new String("Marteau de Thor"), arme.nom().toCharArray());
+        // probleme pour tout les test avec string
+    }
+
+
+    
+
 }
