8fc34264e5c55f4b5dbe908c66fcf4b4046966ff
E174566W <E174566W@localhost>
2018-06-18 10:32:48 +0200

    Etienne

--- src/hearthstone/carte/Arme.java
+++ src/hearthstone/carte/Arme.java
@@ -1,5 +1,7 @@
 package hearthstone.carte;
+
 import com.google.gson.annotations.SerializedName;
+
 /**
  *
  * Classe représentant une carte Arme
@@ -9,69 +11,90 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Arme extends CarteD {
 
-    @SerializedName("durability")
-    private final int durabilite;
+	@SerializedName("durability")
+	private final int durabilite;
 
-    /**
-     * Construit une carte Arme
-     * @param nom nom de la carte
-     * @param mana valeur manna de la carte
-     * @param desc description de la carte
-     * @param rarete rareté de la carte
-     * @param classe classe de la carte
-     * @param urlImage url vers une image de la carte
-     * @param urlImageDoree url vers une version doree de l'image de la carte
-     * @param degats valeur de degats de la carte
-     * @param durabilite valeur de durabilité de la carte
-     */
-    public Arme(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage, String urlImageDoree, int degats, int durabilite) {
-        super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree, degats);
-        this.durabilite = durabilite;
-    }
+	/**
+	 * Construit une carte Arme
+	 * 
+	 * @param nom
+	 *            nom de la carte
+	 * @param mana
+	 *            valeur manna de la carte
+	 * @param desc
+	 *            description de la carte
+	 * @param rarete
+	 *            rareté de la carte
+	 * @param classe
+	 *            classe de la carte
+	 * @param urlImage
+	 *            url vers une image de la carte
+	 * @param urlImageDoree
+	 *            url vers une version doree de l'image de la carte
+	 * @param degats
+	 *            valeur de degats de la carte
+	 * @param durabilite
+	 *            valeur de durabilité de la carte
+	 */
+	public Arme(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage, String urlImageDoree,
+			int degats, int durabilite) {
+		super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree, degats);
+		this.durabilite = durabilite;
+	}
 
-    /**
-     * Construit une carte Arme sans URL d'images
-     * @param nom nom de la carte
-     * @param mana valeur manna de la carte
-     * @param desc description de la carte
-     * @param rarete rareté de la carte
-     * @param classe classe de la carte
-     * @param degats valeur de degats de la carte
-     * @param durabilite valeur de durabilité de la carte
-     */
-    public Arme(String nom, int mana, String desc, Rarete rarete, Classe classe, int degats, int durabilite) {
-        this(nom, mana, desc, rarete, classe, "", "", degats, durabilite);
-    }
+	/**
+	 * Construit une carte Arme sans URL d'images
+	 * 
+	 * @param nom
+	 *            nom de la carte
+	 * @param mana
+	 *            valeur manna de la carte
+	 * @param desc
+	 *            description de la carte
+	 * @param rarete
+	 *            rareté de la carte
+	 * @param classe
+	 *            classe de la carte
+	 * @param degats
+	 *            valeur de degats de la carte
+	 * @param durabilite
+	 *            valeur de durabilité de la carte
+	 */
+	public Arme(String nom, int mana, String desc, Rarete rarete, Classe classe, int degats, int durabilite) {
+		this(nom, mana, desc, rarete, classe, "", "", degats, durabilite);
+	}
 
-    /**
-     *
-     * @return la durabilité de la carte
-     */
-    public int durabilite() {
-        return durabilite;
-    }
+	/**
+	 *
+	 * @return la durabilité de la carte
+	 */
+	public int durabilite() {
+		return durabilite;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Arme)) return false;
-        if (!super.equals(o)) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Arme))
+			return false;
+		if (!super.equals(o))
+			return false;
 
-        Arme arme = (Arme) o;
+		Arme arme = (Arme) o;
 
-        return durabilite == arme.durabilite;
-    }
+		return durabilite == arme.durabilite;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + durabilite;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + durabilite;
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() +
-                ", durabilite='" + durabilite + '\'' ;
-    }
+	@Override
+	public String toString() {
+		return super.toString() + ", durabilite='" + durabilite + '\'';
+	}
 }
