628d3907d2f83f97f6cb4a5f98cdc17b14dcc886
Etienne <etienne.malaboeuf@gmail.com>
2018-06-18 11:45:22 +0200

    Cartes done !

--- src/hearthstone/cartes/Cartes.java
+++ src/hearthstone/cartes/Cartes.java
@@ -16,68 +16,84 @@ import java.util.*;
 
 public class Cartes implements ManipulationCartes {
 
-  /**
-   * créer un paquet de cartes
-   */
-  public Cartes() {
-    // TODO
-  }
+    private Collection<Carte> collectionDeCarte = null;
 
-  /**
-   * créer un paquet de cartes et le remplir à partir des cartes initiales passées
-   * en paramètre
-   * 
-   * @param cartesInitiales les cartes pour remplir le paquet de cartes
-   * @throws CarteDejaPresenteException si une carte de cartes initiales est
-   *                                    présente plusieurs fois
-   */
-  public Cartes(Collection<Carte> cartesInitiales) throws CarteDejaPresenteException {
-    // TODO
-  }
+    /**
+     * créer un paquet de cartes
+     */
+    public Cartes() {
+        collectionDeCarte = new HashSet<>();
+    }
 
-  /**
-   *
-   * @return le paquet de cartes sous la forme d'une collection de cartes au sens
-   *         Collection de Cartes
-   */
-  @Override
-  public Collection<Carte> collection() {
-    // TODO
-    return null;
-  }
+    /**
+     * créer un paquet de cartes et le remplir à partir des cartes initiales passées
+     * en paramètre
+     * 
+     * @param cartesInitiales les cartes pour remplir le paquet de cartes
+     * @throws CarteDejaPresenteException si une carte de cartes initiales est
+     *                                    présente plusieurs fois
+     */
+    public Cartes(Collection<Carte> cartesInitiales) throws CarteDejaPresenteException {
+        this();
 
-  /**
-   * Ajout d'une carte au paquet de cartes
-   * 
-   * @param carte la carte à ajouter
-   * @throws CarteDejaPresenteException si la carte est déjà présente
-   */
-  @Override
-  public void ajouter(Carte carte) throws CarteDejaPresenteException {
-    // TODO
-  }
+        int throwException = 0;
 
-  /**
-   * test si la carte est présente dans le paquet de cartes
-   * 
-   * @param carte la carte à rechercher
-   * @return true si la carte est présente
-   */
-  @Override
-  public boolean estPresente(Carte carte) {
-    // TODO
-    return false;
-  }
+        for (Carte c : cartesInitiales) {
+            if (estPresente(c))
+                ++throwException;
+            ajouter(c);
+        }
+        if (throwException > 0)
+            throw new CarteDejaPresenteException(throwException + " cards could not be added because of duplicates !");
+    }
 
-  /**
-   * supprime la carte du paquet de cartes
-   * 
-   * @param carte la carte à supprimer
-   * @throws CarteAbsenteException si la carte n'est pas dans le paquet de cartes
-   */
-  @Override
-  public void effacer(Carte carte) throws CarteAbsenteException {
-    // TODO
-  }
+    /**
+     *
+     * @return le paquet de cartes sous la forme d'une collection de cartes au sens
+     *         Collection de Cartes
+     */
+    @Override
+    public Collection<Carte> collection() {
+        return collectionDeCarte;
+    }
+
+    /**
+     * Ajout d'une carte au paquet de cartes
+     * 
+     * @param carte la carte à ajouter
+     * @throws CarteDejaPresenteException si la carte est déjà présente
+     */
+    @Override
+    public void ajouter(Carte carte) throws CarteDejaPresenteException {
+        if (estPresente(carte)) {
+            throw new CarteDejaPresenteException("This card is already in this card set !");
+        } else {
+            collectionDeCarte.add(carte);
+        }
+    }
+
+    /**
+     * test si la carte est présente dans le paquet de cartes
+     * 
+     * @param carte la carte à rechercher
+     * @return true si la carte est présente
+     */
+    @Override
+    public boolean estPresente(Carte carte) {
+        return collectionDeCarte.contains(carte);
+    }
+
+    /**
+     * supprime la carte du paquet de cartes
+     * 
+     * @param carte la carte à supprimer
+     * @throws CarteAbsenteException si la carte n'est pas dans le paquet de cartes
+     */
+    @Override
+    public void effacer(Carte carte) throws CarteAbsenteException {
+        if (estPresente(carte))
+            throw new CarteAbsenteException("This card does not exist in this set !");
+        collectionDeCarte.remove(carte);
+    }
 
 }
