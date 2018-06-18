01b4269b5f9b13deb4cb952a07492ed56e8d3d21
Etienne <etienne.malaboeuf@gmail.com>
2018-06-18 10:44:22 +0200

    test

--- src/hearthstone/carte/CarteD.java
+++ src/hearthstone/carte/CarteD.java
@@ -4,62 +4,75 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  *
- * Classe représentant une carte de manière abstraite, permettant de partager l'attribut dégat entre les cartes Arme et les cartes serviteur
+ * Classe représentant une carte de manière abstraite, permettant de partager
+ * l'attribut dégat entre les cartes Arme et les cartes serviteur
  *
  * @author lanoix-a remm-jf
  * @version 1.0
  */
 public abstract class CarteD extends Carte {
 
-    @SerializedName("attack")
-    private final int degats;
+	@SerializedName("attack")
+	private final int degats;
 
-    /**
-     * Construit une carte abstraite
-     * @param nom nom de la carte
-     * @param mana valeur manna de la carte
-     * @param desc description de la carte
-     * @param rarete rareté de la carte
-     * @param classe classe de la carte
-     * @param urlImage url vers une image de la carte
-     * @param urlImageDoree url vers une version doree de l'image de la carte
-     * @param degats valeur de degats de la carte
-     *
-     */
-    CarteD(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage, String urlImageDoree, int degats) {
-        super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree);
-        this.degats = degats;
-    }
+	/**
+	 * Construit une carte abstraite
+	 * 
+	 * @param nom
+	 *            nom de la carte
+	 * @param mana
+	 *            valeur manna de la carte
+	 * @param desc
+	 *            description de la carte
+	 * @param rarete
+	 *            rareté de la carte
+	 * @param classe
+	 *            classe de la carte
+	 * @param urlImage
+	 *            url vers une image de la carte
+	 * @param urlImageDoree
+	 *            url vers une version doree de l'image de la carte
+	 * @param degats
+	 *            valeur de degats de la carte
+	 *
+	 */
+	CarteD(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage, String urlImageDoree,
+			int degats) {
+		super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree);
+		this.degats = degats;
+	}
 
-    /**
-     *
-     * @return la valeur de degats de la carte
-     */
-    public int degats() {
-        return degats;
-    }
+	/**
+	 *
+	 * @return la valeur de degats de la carte
+	 */
+	public int degats() {
+		return degats;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof CarteD)) return false;
-        if (!super.equals(o)) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof CarteD))
+			return false;
+		if (!super.equals(o))
+			return false;
 
-        CarteD carteD = (CarteD) o;
+		CarteD carteD = (CarteD) o;
 
-        return degats == carteD.degats;
-    }
+		return degats == carteD.degats;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + degats;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + degats;
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return super.toString() +
-                ", degats='" + degats + '\'' ;
-    }
+	@Override
+	public String toString() {
+		return super.toString() + ", degats='" + degats + '\'';
+	}
 }
--- src/hearthstone/carte/Serviteur.java
+++ src/hearthstone/carte/Serviteur.java
@@ -11,97 +11,119 @@ import com.google.gson.annotations.SerializedName;
  */
 public class Serviteur extends CarteD {
 
-    @SerializedName("health")
-    private final int pointsDeVie;
+	@SerializedName("health")
+	private final int pointsDeVie;
 
-    @SerializedName("race")
-    private Race race;
+	@SerializedName("race")
+	private Race race;
 
+	/**
+	 * Construit une carte Serviteur
+	 * 
+	 * @param nom
+	 *            nom de la carte
+	 * @param mana
+	 *            valeur manna de la carte
+	 * @param desc
+	 *            description de la carte
+	 * @param rarete
+	 *            rareté de la carte
+	 * @param classe
+	 *            classe de la carte
+	 * @param urlImage
+	 *            url vers une image de la carte
+	 * @param urlImageDoree
+	 *            url vers une version doree de l'image de la carte
+	 * @param degats
+	 *            valeur de degats de la carte
+	 * @param pointsDeVie
+	 *            valeur de points de vie de la carte
+	 * @param race
+	 *            race de la carte
+	 *
+	 */
+	public Serviteur(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage,
+			String urlImageDoree, int degats, int pointsDeVie, Race race) {
+		super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree, degats);
+		this.pointsDeVie = pointsDeVie;
+		this.race = race;
+	}
 
-    /**
-     * Construit une carte Serviteur
-     * @param nom nom de la carte
-     * @param mana valeur manna de la carte
-     * @param desc description de la carte
-     * @param rarete rareté de la carte
-     * @param classe classe de la carte
-     * @param urlImage url vers une image de la carte
-     * @param urlImageDoree url vers une version doree de l'image de la carte
-     * @param degats valeur de degats de la carte
-     * @param pointsDeVie valeur de points de vie de la carte
-     * @param race race de la carte
-     *
-     */
-    public Serviteur(String nom, int mana, String desc, Rarete rarete, Classe classe, String urlImage, String urlImageDoree, int degats, int pointsDeVie, Race race) {
-        super(nom, mana, desc, rarete, classe, urlImage, urlImageDoree, degats);
-        this.pointsDeVie = pointsDeVie;
-        this.race = race;
-    }
+	/**
+	 * Construit une carte Serviteur sans URL d'images
+	 * 
+	 * @param nom
+	 *            nom de la carte
+	 * @param mana
+	 *            valeur manna de la carte
+	 * @param desc
+	 *            description de la carte
+	 * @param rarete
+	 *            rareté de la carte
+	 * @param classe
+	 *            classe de la carte
+	 * @param degats
+	 *            valeur de degats de la carte
+	 * @param pointsDeVie
+	 *            valeur de points de vie de la carte
+	 * @param race
+	 *            race de la carte
+	 *
+	 */
+	public Serviteur(String nom, int mana, String desc, Rarete rarete, Classe classe, int degats, int pointsDeVie,
+			Race race) {
+		this(nom, mana, desc, rarete, classe, "", "", degats, pointsDeVie, race);
+	}
 
-    /**
-     * Construit une carte Serviteur sans URL d'images
-     * @param nom nom de la carte
-     * @param mana valeur manna de la carte
-     * @param desc description de la carte
-     * @param rarete rareté de la carte
-     * @param classe classe de la carte
-     * @param degats valeur de degats de la carte
-     * @param pointsDeVie valeur de points de vie de la carte
-     * @param race race de la carte
-     *
-     */
-    public Serviteur(String nom, int mana, String desc, Rarete rarete, Classe classe, int degats, int pointsDeVie, Race race) {
-        this(nom, mana, desc, rarete, classe, "", "", degats, pointsDeVie, race);
-    }
+	/**
+	 *
+	 * @return la valeur de vie de la carte
+	 */
+	public int pointSDeVie() {
+		return pointsDeVie;
+	}
 
-    /**
-     *
-     * @return la valeur de vie de la carte
-     */
-    public int pointSDeVie() {
-        return pointsDeVie;
-    }
+	/**
+	 *
+	 * @return la race de la carte
+	 */
+	public Race race() {
+		return race;
+	}
 
-    /**
-     *
-     * @return la race de la carte
-     */
-    public Race race(){
-        return race;
-    }
+	public void verifie() {
+		super.verifie();
+		if (race == null)
+			race = Race.ELEMENTAIRE;
+	}
 
-    public void verifie() {
-        super.verifie();
-        if (race == null)
-            race = Race.ELEMENTAIRE;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Serviteur))
+			return false;
+		if (!super.equals(o))
+			return false;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Serviteur)) return false;
-        if (!super.equals(o)) return false;
+		Serviteur serviteur = (Serviteur) o;
 
-        Serviteur serviteur = (Serviteur) o;
+		if (pointsDeVie != serviteur.pointsDeVie)
+			return false;
+		return race == serviteur.race;
+	}
 
-        if (pointsDeVie != serviteur.pointsDeVie) return false;
-        return race == serviteur.race;
-    }
+	@Override
+	public int hashCode() {
+		int result = super.hashCode();
+		result = 31 * result + pointsDeVie;
+		result = 31 * result + race.hashCode();
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + pointsDeVie;
-        result = 31 * result + race.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() +
-                ", pointsDeVie='" + pointsDeVie + '\''+
-                ", race='" + race + '\'' ;
-    }
-
+	@Override
+	public String toString() {
+		return super.toString() + ", pointsDeVie='" + pointsDeVie + '\'' + ", race='" + race + '\'';
+	}
 
 }
