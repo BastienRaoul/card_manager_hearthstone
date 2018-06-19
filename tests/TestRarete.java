1d37e1f9fc1c8af6991576e8359cdf1532c90e47
Bastien <bastien.raoul@etu.univ-nantes.fr>
2018-06-19 09:43:25 +0200

    e174687c

--- tests/TestRarete.java
+++ tests/TestRarete.java
@@ -10,6 +10,37 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
+import java.awt.Color;
+
 public class TestRarete {
 
+    @Test
+    public void testRarete() throws Exception
+    {
+        assertEquals("Mauvaise couleur",Color.ORANGE, Rarete.LEGENDAIRE.gemme());
+    }
+
+    @Test
+    public void testRarete2() throws Exception
+    {
+        assertEquals("Valeur fausse", 1600, Rarete.LEGENDAIRE.valeurCreation());
+    }
+
+    @Test
+    public void testRarete3() throws Exception
+    {
+        assertEquals("Valeur fausse", 400, Rarete.LEGENDAIRE.valeurDesenchantement());
+    }
+
+    @Test
+    public void testRarete4() throws Exception
+    {
+        assertEquals("Valeur fausse", 3200, Rarete.LEGENDAIRE.valeurCreationDoree());
+    }
+
+    @Test
+    public void testRarete5() throws Exception
+    {
+        assertEquals("Valeur fausse", 1600, Rarete.LEGENDAIRE.valeurDesenchantementDoree());
+    }
 }
