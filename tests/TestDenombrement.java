b7883a1675e7d5710fd463465ca0cd332b647bf4
Maxence <maxence.marot@etu.univ-nantes.fr>
2018-06-19 09:42:35 +0200

    Denombrement -probleme negatif

--- tests/TestDenombrement.java
+++ tests/TestDenombrement.java
@@ -1,6 +1,6 @@
 
 import hearthstone.carte.*;
-import hearthstone.cartes.Cartes;
+import hearthstone.cartes.*;
 import hearthstone.cartes.FabriqueJson;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,5 +11,28 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
 public class TestDenombrement {
+    @Test
+    public void testDenombrement1() throws Exception
+    {
+        Denombrement denombrement = new Denombrement(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR),3);
+        assertEquals(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR), denombrement.carte());
+        assertEquals(3, denombrement.nombre());
+    }
 
+    @Test
+    public void testDenombrement2() throws Exception
+    {
+        Denombrement denombrement = new Denombrement(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR),-3);
+        assertEquals(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR), denombrement.carte());
+        assertEquals(-3, denombrement.nombre());
+    }
+
+    @Test
+    public void testDenombrement3() throws Exception
+    {
+        Denombrement denombrement = new Denombrement(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR),3);
+        assertEquals(new Sort("Groot", 5, "Description", Rarete.COMMUNE, Classe.CHASSEUR), denombrement.carte());
+        denombrement.incremente();
+        assertEquals(4, denombrement.nombre());
+    }
 }
