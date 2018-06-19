95f8c3b355e07650322480a582c060716f33d18b
Etienne <etienne.malaboeuf@gmail.com>
2018-06-19 16:58:30 +0200

    test

--- tests/TestDenombrement.java
+++ tests/TestDenombrement.java
@@ -13,8 +13,9 @@ import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
 public class TestDenombrement {
-//On test le constructeur de la classe dénombrement avec comme seul paramètre une carte
-//Par défaut on a donc un exemplaire de la carte donnée en paramètre
+    // On test le constructeur de la classe dénombrement avec comme seul paramètre
+    // une carte
+    // Par défaut on a donc un exemplaire de la carte donnée en paramètre
     @Test
     public void testDenombrement1() throws Exception {
         Carte arme2 = new Arme("Marteau Thor", 10, "MarteauThor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100, 100);
@@ -24,8 +25,10 @@ public class TestDenombrement {
         assertEquals(1, den.nombre());
         assertEquals(arme2, den.carte());
     }
-//On test le constructeur a deux paramètres de la classe dénombrement, avec ici 3 exemplaires de
-//la carte
+
+    // On test le constructeur a deux paramètres de la classe dénombrement, avec ici
+    // 3 exemplaires de
+    // la carte
     @Test
     public void testDenombrement2() throws Exception {
         Carte arme2 = new Arme("Marteau Thor", 10, "MarteauThor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100, 100);
@@ -35,16 +38,20 @@ public class TestDenombrement {
         assertEquals(3, den.nombre());
         assertEquals(arme2, den.carte());
     }
-//On teste ici que l'exception ValeurNegative se déclenche bien, pour qu'il n'y ai pas un nombre
-//D'exemplaires négatif
+
+    // On teste ici que l'exception ValeurNegative se déclenche bien, pour qu'il n'y
+    // ai pas un nombre
+    // D'exemplaires négatif
     @Test(expected = ValeurNegativeException.class)
     public void testDenombrement3() throws Exception {
         Carte arme2 = new Arme("Marteau Thor", 10, "MarteauThor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100, 100);
 
         Denombrement den = new Denombrement(arme2, -1);
     }
-//On test l'incrémentation de la classe dénombrement, pour vérifier qu'il y'a bien un ajout 
-//de l'exemplaire de la carte liée 
+
+    // On test l'incrémentation de la classe dénombrement, pour vérifier qu'il y'a
+    // bien un ajout
+    // de l'exemplaire de la carte liée
     @Test
     public void testDenombrement4() throws Exception {
         Carte arme2 = new Arme("Marteau Thor", 10, "MarteauThor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100, 100);
@@ -56,8 +63,10 @@ public class TestDenombrement {
         assertEquals(2, den.nombre());
         assertEquals(arme2, den.carte());
     }
-//On vérifie ici l'égalité entre le dénombrement de deux cartes correspondant à des variables
-//différentes mais étant égales
+
+    // On vérifie ici l'égalité entre le dénombrement de deux cartes correspondant à
+    // des variables
+    // différentes mais étant égales
     @Test
     public void testDenombrement5() throws Exception {
         Carte arme = new Arme("Marteau Thor", 10, "MarteauThor...", Rarete.LEGENDAIRE, Classe.GUERRIER, 100, 100);
