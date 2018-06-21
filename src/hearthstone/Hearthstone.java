c1f6d472e0c40e15cd36f19861fde33b6424bfaa
Etienne <etienne.malaboeuf@gmail.com>
2018-06-21 11:44:07 +0200

    ocmosdf

--- src/hearthstone/Hearthstone.java
+++ src/hearthstone/Hearthstone.java
@@ -9,6 +9,7 @@ import hearthstone.exception.*;
 import hearthstone.vue.*;
 import hearthstone.*;
 import hearthstone.carte.*;
+
 /**
  * @author lanoix-a remm-jf
  * @version 1.0
@@ -17,11 +18,12 @@ import hearthstone.carte.*;
 public class Hearthstone {
 
     public static void main(String[] args) throws CarteDejaPresenteException, IOException {
-    	Cartes collection = new Cartes(FabriqueJson.lireCartesDepuisFichier("./json/cartes11.json"));
-    	
+        Cartes collection = new Cartes(FabriqueJson.lireCartesDepuisFichier("./json/cartes11.json"));
+
         vueCollection main = new vueCollection(collection);
-        
+
         main.pack();
         main.setVisible(true);
+
     }
 }
