51080c62eed6923863b0da6dc71ae1adf9a744a8
Bastien <bastien.raoul@etu.univ-nantes.fr>
2018-06-22 09:11:40 +0200

    vue

--- src/hearthstone/vue/vueDeck.java
+++ src/hearthstone/vue/vueDeck.java
@@ -1,34 +1,16 @@
 package hearthstone.vue;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Label;
+import java.awt.*;
+import java.awt.datatransfer.*;
+import java.awt.event.*;
 import java.io.IOException;
+import java.util.Vector;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.DropMode;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.ListSelectionModel;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.*;
+import javax.swing.UIManager.*;
 
-import hearthstone.carte.Carte;
-import hearthstone.carte.Classe;
-import hearthstone.cartes.Cartes;
-import hearthstone.cartes.Deck;
+import hearthstone.carte.*;
+import hearthstone.cartes.*;
 import hearthstone.controleur.ctrlChangeClasse;
 import hearthstone.exception.ClasseNeutreException;
 
@@ -42,107 +24,117 @@ public class vueDeck extends vue {
     private JComboBox<Classe> choixClasse = new JComboBox<>();
 
     private JButton creationDeck = new JButton("Terminer");
-    /////////////////////////
-
-    /////
+    /////////////////////////    
 
     public vueDeck(Cartes collection, Deck currentDeck) {
-	super(collection);
+		super(collection);
 
-	mDeck = currentDeck;
+		mDeck = currentDeck;
 
-	classTab.removeAll();
-	classeVoleur();
-	classeNeutre();
+		classTab.removeAll();
+		classeVoleur();
+		classeNeutre();
 
-	/////////////////////////////////
-	subMainRight.setBorder(BorderFactory.createTitledBorder("Création de deck..."));
+		/////////////////////////////////
+		subMainRight.setBorder(BorderFactory.createTitledBorder("CrÃ©ation de deck..."));
 
-	carteList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-	carteList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-	carteList.setVisibleRowCount(-1);
+		carteList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		carteList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		carteList.setVisibleRowCount(-1);
 
-	// Drag & Drop Image dans List
-	carteList.setDragEnabled(true);
-	carteList.setDropMode(DropMode.INSERT);
+		
+		// GhostGlassPane glassPane = new GhostGlassPane();
 
-	// GhostGlassPane glassPane = new GhostGlassPane();
 
-	JScrollPane listeDesDeck = new JScrollPane(carteList);
-	listeDesDeck.setPreferredSize(new Dimension(250, 80));
+		
 
-	////////////////////////////////
 
-	choixClasse.addItem(Classe.GUERRIER);
-	choixClasse.addItem(Classe.DRUIDE);
-	choixClasse.addItem(Classe.CHASSEUR);
-	choixClasse.addItem(Classe.MAGE);
-	choixClasse.addItem(Classe.PALADIN);
-	choixClasse.addItem(Classe.PRETRE);
-	choixClasse.addItem(Classe.CHAMAN);
-	choixClasse.addItem(Classe.DEMONISTE);
-	choixClasse.addItem(Classe.VOLEUR);
 
-	choixClasse.setMaximumRowCount(choixClasse.getModel().getSize());
+		JScrollPane listeDesDeck = new JScrollPane(carteList);
+		listeDesDeck.setPreferredSize(new Dimension(250, 80));
 
-	/////////////////////////////////
+		// Drag & Drop Image dans List
+		carteList.setDragEnabled(true);
+		carteList.setTransferHandler(new ListTransferHandler());
 
-	subMainRight.setLayout(new BorderLayout());
-	subMainRight.add(listeDesDeck, BorderLayout.CENTER);
+		
+		
+	
+		
 
-	JPanel titreDeck = new JPanel();
-	titreDeck.setLayout(new BoxLayout(titreDeck, BoxLayout.Y_AXIS));
 
-	JTextField nomDeck = new JTextField();
-	nomDeck.setText("*Deck*");
-	titreDeck.add(choixClasse);
-	titreDeck.add(nomDeck);
+		////////////////////////////////
 
-	subMainRight.add(titreDeck, BorderLayout.NORTH);
+		choixClasse.addItem(Classe.GUERRIER);
+		choixClasse.addItem(Classe.DRUIDE);
+		choixClasse.addItem(Classe.CHASSEUR);
+		choixClasse.addItem(Classe.MAGE);
+		choixClasse.addItem(Classe.PALADIN);
+		choixClasse.addItem(Classe.PRETRE);
+		choixClasse.addItem(Classe.CHAMAN);
+		choixClasse.addItem(Classe.DEMONISTE);
+		choixClasse.addItem(Classe.VOLEUR);
 
-	////////////////////////////////////
-	JPanel test = new JPanel();
-	test.setLayout(new BoxLayout(test, BoxLayout.X_AXIS));
+		choixClasse.setMaximumRowCount(choixClasse.getModel().getSize());
 
-	JLabel nbCarteDansDeck = new JLabel("15");
-	JLabel sur30 = new JLabel("/30 ");
-	JLabel cartes = new JLabel("cartes");
-	Font font = new Font("Helvetica", Font.BOLD, 18);
-	nbCarteDansDeck.setFont(font);
-	sur30.setFont(font);
+		/////////////////////////////////
 
-	test.add(nbCarteDansDeck);
-	test.add(sur30);
-	test.add(cartes);
-	test.add(Box.createHorizontalStrut(48));
-	test.add(creationDeck);
+		subMainRight.setLayout(new BorderLayout());
+		subMainRight.add(listeDesDeck, BorderLayout.CENTER);
 
-	subMainRight.add(test, BorderLayout.SOUTH);
+		JPanel titreDeck = new JPanel();
+		titreDeck.setLayout(new BoxLayout(titreDeck, BoxLayout.Y_AXIS));
 
-	/////////////////////////////////
+		JTextField nomDeck = new JTextField();
+		nomDeck.setText("*Deck*");
+		titreDeck.add(choixClasse);
+		titreDeck.add(nomDeck);
 
-	choixClasse.addActionListener(new ctrlChangeClasse(this));
+		subMainRight.add(titreDeck, BorderLayout.NORTH);
 
-	/////////////////////////////////
+		////////////////////////////////////
+		JPanel test = new JPanel();
+		test.setLayout(new BoxLayout(test, BoxLayout.X_AXIS));
 
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	// this.setLocation(300, 300);
+		JLabel nbCarteDansDeck = new JLabel("15");
+		JLabel sur30 = new JLabel("/30 ");
+		JLabel cartes = new JLabel("cartes");
+		Font font = new Font("Helvetica", Font.BOLD, 18);
+		nbCarteDansDeck.setFont(font);
+		sur30.setFont(font);
 
-	this.setPreferredSize(new Dimension(X, Y));
-	setSize(X, Y);
+		test.add(nbCarteDansDeck);
+		test.add(sur30);
+		test.add(cartes);
+		test.add(Box.createHorizontalStrut(48));
+		test.add(creationDeck);
 
-	try {
-	    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		if ("Nimbus".equals(info.getName())) {
-		    UIManager.setLookAndFeel(info.getClassName());
-		    break;
+		subMainRight.add(test, BorderLayout.SOUTH);
+
+		/////////////////////////////////
+
+		choixClasse.addActionListener(new ctrlChangeClasse(this));
+
+		/////////////////////////////////
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// this.setLocation(300, 300);
+
+		this.setPreferredSize(new Dimension(X, Y));
+		setSize(X, Y);
+
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+			if ("Nimbus".equals(info.getName())) {
+				UIManager.setLookAndFeel(info.getClassName());
+				break;
+			}
+			}
+		} catch (Exception e) {
+			System.out.println("No nimbus");
 		}
-	    }
-	} catch (Exception e) {
-	    System.out.println("No nimbus");
-	}
 
-	setVisible(true);
+		setVisible(true);
     }
 
     //////////////////////////////////////////
