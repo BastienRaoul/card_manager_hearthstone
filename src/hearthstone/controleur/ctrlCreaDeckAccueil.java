416530da3a46240755782465b621030ef9d32efa
Maxence <maxence.marot@etu.univ-nantes.fr>
2018-06-22 10:35:36 +0200

    ctrl

--- src/hearthstone/controleur/ctrlCreaDeckAccueil.java
+++ src/hearthstone/controleur/ctrlCreaDeckAccueil.java
@@ -1,4 +1,4 @@
-package hearthstone.controleur;
+/*package hearthstone.controleur;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,12 +6,17 @@ import java.awt.event.ActionListener;
 import hearthstone.vue.vueDeck;
 import hearthstone.vue.vueCollection;
 
+import hearthstone.cartes.Cartes;
+import hearthstone.cartes.Deck;
+
 public class ctrlCreaDeckAccueil implements ActionListener {
 
-    vueDeck mVue = null;
+	vueDeck mVue = null;
+	Cartes mCollection = null;
 
-    public ctrlCreaDeckAccueil(vueDeck vue) {
+    public ctrlCreaDeckAccueil(vueDeck vue, Cartes collection) {
 	mVue = vue;
+	mCollection = collection;
     }
 
     @Override
@@ -24,7 +29,16 @@ public class ctrlCreaDeckAccueil implements ActionListener {
 	vueCollection main = new vueCollection(mVue.collection);
 	main.isWindowOpen = false;
 
+	if(mVue.getnomDeck() != null )
+	{
+		mCollection.ajouterDeck(mVue.getClasse(), 30, mVue.getnomDeck());
+	}
+	else
+	{
+		mCollection.ajouterDeck(mVue.getClasse(), 30, "Nouveau deck");
+	}
+
 	main.pack();
 	main.setVisible(true);
     }
-}
+}*/
